d191266cdd4e07a18884a5fb1d05edda7b7f4d7c
Petri Kainulainen <petri.kainulainen@gmail.com>
2023-11-27 17:21:25 +0200

    Improve eslint config and make cosmetic changes

--- 1-styled-components/.eslintrc.cjs
+++ 1-styled-components/.eslintrc.cjs
@@ -1,18 +1,26 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2021: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'prettier',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'package-lock.json'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@typescript-eslint', 'react-refresh', 'prettier'],
   rules: {
+    'semi-style': ['error', 'last'],
+    'no-console': 1,
+    indent: [0], // Let Prettier to handle indent
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
